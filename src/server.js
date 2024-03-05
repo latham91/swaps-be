43110d84bb1866ea8b4872bd4f383906e82ef238
Aaron Latham <latham91@icloud.com>
2024-03-05 12:06:45 +0000

    chore: initial setup

---
+++ src/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: ["http://localhost:5001"] }));
+
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+// Health check
+app.get("/api/health", (req, res) => {
+    try {
+        return res.status(200).json({ message: "API is healthy" });
+    } catch (error) {
+        return res.status(500).json({ message: "API not working" });
+    }
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});

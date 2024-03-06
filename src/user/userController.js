e65bf38d19fed6aa4392ce64a7b8be686bbe30bb
Aaron Latham <latham91@icloud.com>
2024-03-06 12:38:40 +0000

    chore: deleted stuff in user controller

--- src/user/userController.js
+++ src/user/userController.js
@@ -1,10 +1,3 @@
 require("dotenv").config();
 const User = require("./userModel");
 const jwt = require("jsonwebtoken");
-
-exports.loginUser = async (req, res) => {
-    try {
-    } catch (error) {
-        return res.status(500).json({ success: false, message: "Server error", error: error.message });
-    }
-};

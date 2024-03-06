efd1ddfec95f852dc033b27ad31f6b6ac97d236b
Aaron Latham <latham91@icloud.com>
2024-03-06 12:33:59 +0000

    enhancement: BE4 & BE5  create auth middleware functions, hashPassword and comparePassword

--- src/user/userController.js
+++ src/user/userController.js
@@ -1,3 +1,10 @@
 require("dotenv").config();
 const User = require("./userModel");
 const jwt = require("jsonwebtoken");
+
+exports.loginUser = async (req, res) => {
+    try {
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Server error", error: error.message });
+    }
+};

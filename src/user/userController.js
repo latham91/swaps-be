eee2d7a2b4ca90804ef75d5a9f168ad25a64b963
leotanswell <leandro8tanswell@outlook.com>
2024-03-06 12:03:45 +0000

    enhancement: create sign-up function

--- src/user/userController.js
+++ src/user/userController.js
@@ -1,3 +1,34 @@
 require("dotenv").config();
 const User = require("./userModel");
 const jwt = require("jsonwebtoken");
+
+exports.signupUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return res
+                .status(400)
+                .json({ success: false, message: "Account already exists" });
+        }
+
+        const user = await User.create({ username, email, password });
+
+        return res.status(201).json({
+            success: true,
+            message: "Account signed up successfully",
+            user,
+        });
+    } catch (error) {
+        return res
+            .status(500)
+            .json({
+                success: false,
+                message: "Server error",
+                source: "signupUser",
+                error: error.message,
+            });
+    }
+};

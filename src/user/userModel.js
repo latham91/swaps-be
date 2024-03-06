e7db9b3e192a5349e5508967fdab0c5680192297
leotanswell <leandro8tanswell@outlook.com>
2024-03-06 11:00:57 +0000

    enhancement: BE7 create user model

---
+++ src/user/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import isEmail from "validator/lib/isEmail";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [isEmail, "Please enter a valid Email"],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    listingsArray: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Listing",
+        },
+    ],
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;

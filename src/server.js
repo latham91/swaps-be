09d8c88c97619e2e755657ee6db1fb7d30872944
Aaron Latham <latham91@icloud.com>
2024-03-07 17:59:03 +0000

    enhancement: create logoutUser function in userController

--- src/server.js
+++ src/server.js
@@ -15,26 +15,22 @@ const port = process.env.PORT || 5001;
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-        origin: [
-            "http://localhost:5001",
-            "https://swaps-fe.netlify.app",
-            "http://localhost:5173",
-        ],
-    })
+  cors({
+    origin: ["http://localhost:5001", "https://swaps-fe.netlify.app", "http://localhost:5173"],
+  })
 );
 
 if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
 
 // Health check
 app.get("/api/health", (req, res) => {
-    try {
-        return res.status(200).json({ message: "API is healthy" });
-    } catch (error) {
-        return res.status(500).json({ message: "API not working" });
-    }
+  try {
+    return res.status(200).json({ message: "API is healthy" });
+  } catch (error) {
+    return res.status(500).json({ message: "API not working" });
+  }
 });
 
 // Routes
@@ -43,6 +39,6 @@ app.use("/api/listings", listingRoutes);
 
 // Start server
 app.listen(port, () => {
-    dbConnect();
-    console.log(`Server is listening on port ${port}`);
+  dbConnect();
+  console.log(`Server is listening on port ${port}`);
 });
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const { comparePassword, hashPassword } = require("../middleware/auth");
+const verifyJwt = require("../middleware/verifyJwt");
 const { signupUser, loginUser } = require("./userController");
 
 router.post("/signup", hashPassword, signupUser);
 router.post("/login", comparePassword, loginUser);
+router.post("/logout", verifyJwt, logoutUser);
 
 module.exports = router;

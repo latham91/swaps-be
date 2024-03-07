428260ae6aa7bdcfd910448f278c61dfd78fb2a4
Aaron Latham <latham91@icloud.com>
2024-03-07 18:03:03 +0000

    chore: added logoutUser import to userRoutes

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { comparePassword, hashPassword } = require("../middleware/auth");
 const verifyJwt = require("../middleware/verifyJwt");
-const { signupUser, loginUser } = require("./userController");
+const { signupUser, loginUser, logoutUser } = require("./userController");
 
 router.post("/signup", hashPassword, signupUser);
 router.post("/login", comparePassword, loginUser);

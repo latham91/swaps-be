74f126cce3dbb20f18c89d3e8dfa7c4db25f2ae9
leotanswell <leandro8tanswell@outlook.com>
2024-03-11 12:07:06 +0000

    delete user route

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
 const { comparePassword, hashPassword } = require("../middleware/auth");
 const verifyJwt = require("../middleware/verifyJwt");
-const { signupUser, loginUser, logoutUser, userSession } = require("./userController");
+const {
+    signupUser,
+    loginUser,
+    logoutUser,
+    userSession,
+    deleteUser,
+} = require("./userController");
 
 router.post("/signup", hashPassword, signupUser);
 router.post("/login", comparePassword, loginUser);
 router.post("/logout", verifyJwt, logoutUser);
 router.get("/session", verifyJwt, userSession);
+router.delete("/delete", deleteUser);
 
 module.exports = router;
